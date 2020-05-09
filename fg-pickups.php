ca6d2cb86ac4ff68fe525a4b7ce5074a3326468d
Vasilis Koutsopoulos <vaskou@hotmail.com>
2020-05-09 11:34:27 +0300

    -Init

---
+++ fg-pickups.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @wordpress-plugin
+ * Plugin Name:       FremeditiGuitars - Pickups
+ * Description:       FremeditiGuitars - Pickups Post Type
+ * Version:           1.0.0
+ * Author:            Vasilis Koutsopoulos
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       fg-pickups
+ * Domain Path:       /languages
+ */
+
+defined( 'ABSPATH' ) or die();
+
+define( 'FG_PICKUPS_VERSION', '1.0.0' );
+define( 'FG_PICKUPS_PLUGIN_DIR_PATH', plugin_dir_path( __FILE__ ) );
+define( 'FG_PICKUPS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+define( 'FG_PICKUPS_PLUGIN_DIR_NAME', basename( FG_PICKUPS_PLUGIN_DIR_PATH ) );
+define( 'FG_PICKUPS_PLUGIN_URL', plugins_url( FG_PICKUPS_PLUGIN_DIR_NAME ) );
+
+
+
